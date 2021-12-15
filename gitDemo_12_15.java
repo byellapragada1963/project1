5d7d12fe41a86e6c3c6680d6666dd146f2fff43d
byellapragada1963 <96197039+byellapragada1963@users.noreply.github.com>
2021-12-15 13:11:39 -0500

    Add files via upload

---
+++ gitDemo_12_15.java
@@ -0,0 +1,13 @@
+public class gitDemo_12_15 {
+
+
+
+public static void main( String[] args) {
+	int a = 1;
+	int b=2;
+   	int sum = a + b;
+   	System.out.println( "This is a sample file for Git demo");
+
+}
+
+}
